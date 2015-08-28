96df1b1aee92f3a779cd7cbac92bb92c27bb32eb
Michael V <michaelv@attunix.com>
2015-08-28 15:27:24 -0700

    source

---
+++ GitDeployWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitDeployWebApp.Startup))]
+namespace GitDeployWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
