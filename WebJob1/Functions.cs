c56c5fa8480723431685c76089dd327530bd2ae7
Michael V <michaelv@attunix.com>
2015-09-01 10:24:41 -0700

    adding webjob in git

---
+++ WebJob1/Functions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJob1
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log) {
+            try {
+                var db = new Db();
+                db.Messages.Add(new Message {Body = "Y " + message, ReceivedOn = DateTime.UtcNow});
+                db.SaveChanges();
+            }
+            catch (Exception e) {
+                log.WriteLine("Exception: " + e.Message);
+            }
+            log.WriteLine("X:" + message);
+        }
+    }
+
+    public class Db : DbContext
+    {
+        public Db()
+            : base("name=DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime ReceivedOn { get; set; }
+    }
+ }
